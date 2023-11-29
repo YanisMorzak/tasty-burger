7df509d27497c90609e5c7e64e54072065833623
Yanis <morzak78@gmail.com>
2023-11-29 20:36:39 +0100

    🎨  chore(F01): LOGIN > improve function naming

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -3,30 +3,24 @@ import React, {useState} from 'react'
 export default function LoginPage() {
     const [input, setInput] = useState("")
 
-    const submitDisplay = (e) => {
+    const handleChange = (e) => {
     e.preventDefault()
     setInput(e.target.value)  
   
     }
   
-    const accesArea = (e) => {
+    const handleSubmit = (e) => {
       e.preventDefault()
-      
-      if (input == "") {
-        alert("veuillez renseigner votre prénom")
-      } else {
         alert("Bonjour " + input);
-        setInput("")
-        
-      }
-     
+        setInput("")   
     }
+    
   return (
 <div>
     <h1>Bienvenu chez nous !</h1>
     <h3>Connectez vous</h3>
-    <form action="" >
-      <input type="text" placeholder='Entrez votre prénom' onChange={submitDisplay} value={input} /><button onClick={accesArea}>Accèdez à votre espace</button>
+    <form action="" onSubmit={handleSubmit} >
+      <input type="text" placeholder='Entrez votre prénom' onChange={handleChange} value={input} required/><button >Accèdez à votre espace</button>
     </form> 
     </div>
   )
