f560e4c4f746a2510e0bb3d42451716c6fe170c6
Yanis <morzak78@gmail.com>
2023-12-01 10:46:39 +0100

    ✨ F02: APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import LoginPage from './components/pages/login/LoginPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import OrderPage from './components/pages/order/OrderPage';
+import NotFound from './components/NotFound';
 
 const App = () => {
 
- 
   return (
-    <LoginPage />
+       
+    <BrowserRouter>    
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>   
+    </BrowserRouter>
+   
   );
 };
 
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function OrderPage() {
+  return (
+    <div>
+        <h1>Bonjour</h1>
+        <Link to="/"><button>Déconnexion</button></Link>
+    </div>
+  )
+}
