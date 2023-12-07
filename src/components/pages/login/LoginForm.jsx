a14c3a52d235ab01fc3bb24316374e07a29f6003
Yanis <morzak78@gmail.com>
2023-12-07 20:05:16 +0100

    🚚 F03: LOGIN > LoginForm > Remove TextInput in reusable-ui folder

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { styled } from 'styled-components'
 import { IoChevronForward } from "react-icons/io5";
 import { BsPersonCircle } from "react-icons/bs";
-import TextInput from './TextInput';
+import TextInput from '../../reusable-ui/TextInput';
 
 export default function LoginForm() {
     const [input, setInput] = useState("")
