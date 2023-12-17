9fdba3e84df4ee7fb8aa3c21abb78d1a519ee7f4
Yanis <morzak78@gmail.com>
2023-12-17 16:46:15 +0100

    💄 F04: ORDER > Add primary color to background

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -21,7 +21,7 @@ export default function OrderPage() {
 }
 
 const OrderPageStyled = styled.div`
-  background: orange;
+  background: ${theme.colors.primary};
   height: 100vh;
   display: flex;
   justify-content: center;
