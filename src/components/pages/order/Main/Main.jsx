c0991b6d1086fa907cfa4f8f86107b72ebbdfdbe
Yanis <morzak78@gmail.com>
2023-12-19 17:29:24 +0100

    🚧 F05: ORDER > Menu > Add grid layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -21,6 +21,7 @@ const MainStyled = styled.div`
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
     display: grid;
     grid-template-columns: 1fr;
+    overflow-y: scroll;
 
     /* .basket{
       background: pink;
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled } from 'styled-components';
+import { fakeMenu2 } from '../../../../fakeData/fakeMenu';
 
 export default function Menu() {
+    const [products, setProducts] = useState(fakeMenu2)
   return (
-    <MenuStyled className="menu">menu</MenuStyled>
+    <MenuStyled className="menu">
+        {products.map((produit) => {
+            return <div className='produit'>{produit.title}</div>
+        })}
+        </MenuStyled>
   )
 }
 const MenuStyled = styled.div`
     background: purple;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 60px;
+    padding: 50px 50px 150px;
+    justify-items: center;
+    
+    .produit{
+        background: red;
+        width: 240px;
+        height: 330px;
+    }
 
 `;
