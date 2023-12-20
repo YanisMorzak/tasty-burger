336bae367d088125cd83f17cb7e9f5e5c1ebc4fd
Yanis <morzak78@gmail.com>
2023-12-20 15:28:38 +0100

    💄 F05: ORDER > Menu > make it responsive

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -21,7 +21,7 @@ const MenuStyled = styled.div`
     background: ${theme.colors.background_white};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-row-gap: 60px;
     padding: 50px 50px 150px;
     justify-items: center;
