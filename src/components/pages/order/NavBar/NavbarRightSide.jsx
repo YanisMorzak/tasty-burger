9fe2f4f403931442a17f5327e81ac3e572683421
Yanis <morzak78@gmail.com>
2023-12-21 19:08:46 +0100

    ✨ F06: NAVBAR > Add notification toast on admin button click

--- src/components/pages/order/NavBar/NavbarRightSide.jsx
+++ src/components/pages/order/NavBar/NavbarRightSide.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styled } from 'styled-components';
 import Profile from './Profile';
 import ToggleButton from '../../../reusable-ui/ToggleButton';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { theme } from '../../../../theme';
 
 export default function NavbarRightSide({username}) {
+
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if(isModeAdmin) {
+    toast.info("Mode admin activé", {
+    theme: "dark",
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })}
+  setIsModeAdmin(!isModeAdmin)
+}
   return (
-    <NavbarRightSideStyled className='right-side'>
-      <ToggleButton labelIfUnchecked="ACTIVER LE MODE ADMIN" labelIfChecked="DESACTIVER LE MODE ADMIN" />
+    <NavbarRightSideStyled>
+      <ToggleButton labelIfUnchecked="ACTIVER LE MODE ADMIN" labelIfChecked="DESACTIVER LE MODE ADMIN" onToggle={displayToastNotification} />
     <Profile username={username}/>
+    <ToastContainer className="toaster" bodyClassName="body-toast"/>
   </NavbarRightSideStyled>
   )
 }
@@ -19,5 +40,22 @@ const NavbarRightSideStyled = styled.div`
     /* .admin-button {
         background: lightblue;
     } */
+    .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 
 `;
