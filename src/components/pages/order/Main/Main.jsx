1369cc88c878beb080ff574ca464551034b56d8b
Yanis <morzak78@gmail.com>
2023-12-29 16:56:04 +0100

    💄 F07: MAIN > Add layout with admin

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -8,23 +8,44 @@ export default function Main() {
   return (
     <MainStyled className="main">
       {/* <div className="basket">basket</div> */}
-      <Menu />
+     <div className='menu-admin'>
+       <Menu />
+       <div className="admin">Admin</div>
+     </div>
     </MainStyled>
   )
 }
 
 const MainStyled = styled.div`
+
     background: ${theme.colors.background_white};
-    flex: 1;
+    height: calc(95vh - 10vh);
     border-bottom-left-radius: ${theme.borderRadius.extraRound};
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
     display: grid;
     grid-template-columns: 1fr;
-    overflow-y: scroll;
+    
 
     /* .basket{
       background: pink;
     } */
     
+    .menu-admin{
+      border: 1px solid green;
+      position: relative;
+      overflow-y: hidden;
+      border-bottom-left-radius: ${theme.borderRadius.extraRound};
+      border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+      .admin{
+        background: red;
+        height: 250px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    }
+   
 `;
