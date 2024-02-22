d4eb7868cb0cf888dc9b6b71f1e86828e8b2e411
Yanis <morzak78@gmail.com>
2024-02-22 17:17:42 +0100

    🔧 F13: Add firebase-config.js

---
+++ src/api/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); // pour se connecter/s'identifier à notre compte firebase
+export const db = getFirestore(app); // pour se connecter à notre BDD firestore
